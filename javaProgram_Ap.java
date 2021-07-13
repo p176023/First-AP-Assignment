dee0698ae3799072c94779a6cfd99c4fee63d1e5
p176023 <p176023@nu.edu.pk>
2021-07-13 13:13:08 +0500

    first java program

---
+++ javaProgram_Ap.java
@@ -0,0 +1,8 @@
+// A Java program to print "Hello World" 
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Sir"); 
+    }
+}
